5d65ff7951e9c162eaa10d8cb7e3ded0fa135fd1
ufochuxian <chen853409335@gmail.com>
2021-04-12 23:24:17 +0800

    CorrcumentHashMap的replace函数的使用

--- src/com/thread/test/ConcruentHashMapaTest.java
+++ src/com/thread/test/ConcruentHashMapaTest.java
@@ -4,47 +4,51 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class ConcruentHashMapaTest {
 
-    public static void main(String[] args) throws InterruptedException {
+    public  static ConcurrentHashMap<String, Integer> hashMap = new ConcurrentHashMap<>();
 
-        CalRunnable calRunnable = new CalRunnable();
-        Thread thread1 = new Thread(calRunnable);
-        Thread thread2 = new Thread(calRunnable);
+    public static void main(String[] args) throws InterruptedException {
+        hashMap.put("value",0);
+        Thread thread1 = new Thread(new CalRunnable());
+        Thread thread2 = new Thread(new CalRunnable());
         thread1.start();
         thread2.start();
         thread1.join();
         thread2.join();
-        calRunnable.print();
-
+        System.out.println("hashMap:" + hashMap.get("value"));
     }
 
 
-}
+    static class CalRunnable implements Runnable {
 
+        @Override
+        public void run() {
 
-class CalRunnable implements Runnable {
-
-    ConcurrentHashMap<String, Integer> hashMap = new ConcurrentHashMap<>();
-
-    @Override
-    public void run() {
-
-        for (int i = 0; i < 1000; i++) {
-            //这里如果不加上synchronized关键字的话，那么即便使用CorrcurrentHashMap，那么也不是线程安全的
-            //因为CorrcurrentHashMap只能保证单独的get,put的线程安全，无法保证一组操作的线程安全。
-            synchronized (this) {
-                Integer value = hashMap.get("value");
-                if(value == null) {
-                    value = 0;
+            for (int i = 0; i < 1000; i++) {
+                //这里如果不加上synchronized关键字的话，那么即便使用CorrcurrentHashMap，那么也不是线程安全的
+                //因为CorrcurrentHashMap只能保证单独的get,put的线程安全，无法保证一组操作的线程安全。
+//                hashMap.put("value", value+1);
+                //这里如果不使用锁，那么可以使用ConcruentHashMap的replace，这个方法可以保证原子性操作
+                while (true) {
+                    Integer value = hashMap.get("value");
+                    int newValue = value + 1;
+                    String threadName = Thread.currentThread().getName();
+                    System.out.println("value:" + value + ",newValue" + newValue + ",threadName:" + threadName);
+                    boolean updateValueSuccess = hashMap.replace("value", value, newValue);
+                    if (updateValueSuccess) {
+                        //replace方法，会有一个返回值，只有保证更新成功了，那么退出。否则会等待，再次尝试更新
+                        break;
+                    }
                 }
-                String threadName = Thread.currentThread().getName();
-                System.out.println("value:" + value + ",threadName:" + threadName);
-                hashMap.put("value", value+1);
             }
+
         }
 
+        public void print() {
+            System.out.println("i的值:" + hashMap.get("value"));
+        }
     }
 
-    public void print() {
-        System.out.println("i的值:" + hashMap.get("value"));
-    }
+
 }
+
+
